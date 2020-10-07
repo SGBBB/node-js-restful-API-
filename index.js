a666317e1e48384620d1b46f17378257854aa77e
SAGAR_Bora <64132596+SGBBB@users.noreply.github.com>
2020-10-07 21:08:58 +0530

    Done upto sending mails from  gmail service using nodemailer module

--- index.js
+++ index.js
@@ -1,5 +1,39 @@
 // Import express
 let express = require('express')
+const nodemailer = require('nodemailer');
+
+var mail = nodemailer.createTransport({
+     service: 'gmail',
+     auth: {
+          user: 'sagarbora.2k01@gmail.com',
+          pass: '' 
+     }
+});
+
+var mailOptions = {
+     from: 'sagarbora.2k01@gmail.com', // Sender address
+     to: 'sagarbora1857@gmail.com',      // List of recipients
+     subject: 'Sending Email using Node.js',
+     text: 'Have the most fun you can in a car. Get your Tesla today!', // Plain text body
+     html: '<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>',
+
+     attachments: [
+          { // Use a URL as an attachment
+               filename: 'your-testla.png',
+               path: 'https://media.gettyimages.com/photos/view-of-tesla-model-s-in-barcelona-spain-on-september-10-2018-picture-id1032050330?s=2048x2048'
+          }
+     ]
+}
+
+
+mail.sendMail(mailOptions, function (error, info) {
+     if (error) {
+          console.log(error);
+     } else {
+          console.log('Email sent: ' + info.response);
+     }
+});
+
 // Initialize the app
 let app = express();
 const bodyParser = require('body-parser');
